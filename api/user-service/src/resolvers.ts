1f32e9170de77dd53db1b13e68d63227e7a5161e
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-11-06 22:35:30 -0300

    Setup prisma in user-service

--- api/user-service/src/resolvers.ts
+++ api/user-service/src/resolvers.ts
@@ -1,33 +1,28 @@
+import { DateResolver } from 'graphql-scalars';
+import { PrismaClient } from '@prisma/client';
 import { Resolvers } from '@generated/resolvers-types';
 
-export const users = [
-  {
-    id: '1',
-    username: 'Person Wun',
-    walletId: '000-001',
-  },
-  {
-    id: '2',
-    username: 'User Tooh',
-    walletId: '000-002',
-  },
-];
+const prisma = new PrismaClient();
 
-//Define resolvers
 export const resolvers: Resolvers = {
+  Date: DateResolver,
   Query: {
-    //Resolver for 'user' queries by ID
-    user: async (parent, { id }) => {
-      const userById = users.find(user => user.id === id);
-      return userById!;
+    users: async () => {
+      return prisma.user.findMany();
     },
+    // user: async (parent, { id }) => {
+    //   const userById = users.find(user => user.id === id);
+    //   return userById!;
+    // },
   },
   User: {
-    //Reference resolver - used by services querying user data
-    //Resolver for 'user' queries by wallet ID
-    __resolveReference(reference) {
-      const { walletId } = reference;
-      return users.find(user => user.walletId === walletId)!;
+    __resolveReference: reference => {
+      const { id } = reference;
+      return null;
     },
+    // __resolveReference(reference) {
+    //   const { walletId } = reference;
+    //   return users.find(user => user.walletId === walletId)!;
+    // },
   },
 };
