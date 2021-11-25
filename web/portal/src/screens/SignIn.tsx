fd75148173e45e0c369298f799145318e95db71d
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-11-24 23:52:22 -0300

    Implement signin and signup

--- web/portal/src/screens/SignIn.tsx
+++ web/portal/src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
-import { Form, Input, Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, Divider, message } from 'antd';
+import { MailOutlined, LockOutlined } from '@ant-design/icons';
 import { useAuth } from '../hooks/useAuth';
 
 export function SignIn() {
+  const navigate = useNavigate();
   const { signin } = useAuth();
 
   const onFinish = useCallback(
@@ -11,36 +13,58 @@ export function SignIn() {
       try {
         await signin(data);
       } catch (err) {
-        alert(err);
+        message.error('Invalid email or password');
       }
     },
     [signin]
   );
 
   return (
-    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} onFinish={onFinish}>
-      <Form.Item
-        name="email"
-        label="Email"
-        rules={[
-          { required: true, message: 'Please input your email!' },
-          { type: 'email', message: 'Please input a valid email!' },
-        ]}
-      >
-        <Input prefix={<UserOutlined />} />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input type="password" />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <div
+      style={{
+        display: 'grid',
+        width: '100vw',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Form onFinish={onFinish} style={{ width: '400px' }} layout="vertical">
+        <h1 style={{ textAlign: 'center' }}>Login</h1>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'Please input a valid email!' },
+          ]}
+        >
+          <Input prefix={<MailOutlined />} />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input type="password" prefix={<LockOutlined />} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Login
+          </Button>
+        </Form.Item>
+        <Divider>or</Divider>
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <Button
+            type="default"
+            htmlType="button"
+            style={{ width: '100%' }}
+            onClick={() => navigate('/signup')}
+          >
+            Create new account
+          </Button>
+        </div>
+      </Form>
+    </div>
   );
 }
--- web/portal/src/screens/SignUp.tsx
+++ web/portal/src/screens/SignUp.tsx
@@ -1,7 +1,89 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, message, Divider } from 'antd';
+import {
+  UserOutlined,
+  LockOutlined,
+  MailOutlined,
+  IdcardOutlined,
+} from '@ant-design/icons';
+import { useAuth } from '../hooks/useAuth';
+
 export function SignUp() {
+  const navigate = useNavigate();
+  const { signup } = useAuth();
+
+  const onFinish = useCallback(
+    async data => {
+      try {
+        await signup(data);
+      } catch (err) {
+        message.error('Email or CPF already taken');
+      }
+    },
+    [signup]
+  );
+
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div
+      style={{
+        display: 'grid',
+        width: '100vw',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Form onFinish={onFinish} style={{ width: '400px' }} layout="vertical">
+        <h1 style={{ textAlign: 'center' }}> Create new account</h1>
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+          <Input prefix={<UserOutlined />} />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'Please input a valid email!' },
+          ]}
+        >
+          <Input prefix={<MailOutlined />} />
+        </Form.Item>
+        <Form.Item
+          name="cpf"
+          label="CPF"
+          rules={[{ required: true, message: 'Please input your CPF!' }]}
+        >
+          <Input prefix={<IdcardOutlined />} />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input type="password" prefix={<LockOutlined />} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Register
+          </Button>
+        </Form.Item>
+        <Divider>or</Divider>
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <Button
+            type="default"
+            htmlType="button"
+            style={{ width: '100%' }}
+            onClick={() => navigate('/signin')}
+          >
+            Login
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 }
