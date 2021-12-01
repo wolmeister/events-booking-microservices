e33c378449cccf9c79d2225b2dab906e72605ae0
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-12-01 20:43:27 -0300

    Add pwa support

--- web/checkin/src/main.tsx
+++ web/checkin/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 
 import { UrqlProvider } from './providers/UrqlProvider';
 import { App } from './App';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('onNeedRefresh', arguments);
+  },
+  onOfflineReady() {
+    console.log('onOfflineReady', arguments);
+  },
+  onRegistered() {
+    console.log('onOfflineReady', arguments);
+  },
+});
+
+console.log('updateSw', updateSW);
 
 ReactDOM.render(
   <React.StrictMode>
--- web/checkin/vite.config.ts
+++ web/checkin/vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Events Booking Checkin',
+        short_name: 'Events Booking Checkin',
+        description: 'Events Booking Checkin',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3300,
   },
